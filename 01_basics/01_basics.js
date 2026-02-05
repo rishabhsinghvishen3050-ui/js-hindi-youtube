fa4cd30d4982b0c1c7293e79207ca4729c43eac7
rishabhsinghvishen3050-ui <rishabhsinghvishen3050@gmail.com>
2026-02-05 15:11:00 +0000

    container config done

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=2 not allowed
+console.log(accountId);
+accountCity="Bengaluru"
+accountPassword="657585"
+accountEmail="rishabh@google.com"
+let accountNumber
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountNumber])
