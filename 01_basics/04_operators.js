1ae610ce7c87552535c09ae932ee0bf0b9562c1d
rishabhsinghvishen3050-ui <rishabhsinghvishen3050@gmail.com>
2026-02-05 17:19:15 +0000

    nothing

---
+++ 01_basics/04_operators.js
@@ -0,0 +1,14 @@
+let value=3
+let str1="rishabh"
+let str2="singh"
+console.log(str1+str2)
+
+ //something confusing is written down
+ console.log("1"+2);
+ console.log(1+"2");
+ console.log("1"+2+2); //all will be converted into string and then concatenated
+ console.log(1+2+"2"); //first addtion will take place and then string concatenation
+
+ let gamecounter=99
+ gamecounter++;
+ console.log(gamecounter)
