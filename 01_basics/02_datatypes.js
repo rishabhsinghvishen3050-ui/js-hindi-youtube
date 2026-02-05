94cd1df7f44bbdd3801f2e6cec7cd6243dd963f4
rishabhsinghvishen3050-ui <rishabhsinghvishen3050@gmail.com>
2026-02-05 15:46:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Rishabh");// depends on us to use ";" or not
+
+let nam="Rishabh"
+let age = 18
+let isLoggedIn=false
+
+// range of integers is around 2 to power 53 or something
+/*for very large integers we should use datatype "bigint"
+ string=> ""
+ boolean=> true/false
+ null=> standalone value
+ undefined=>
+    symbol datatype use to unique values and whatever
+ object*/
+  console.log(typeof nam);
+  console.log(typeof isLoggedIn);
+  console.log(typeof null);
