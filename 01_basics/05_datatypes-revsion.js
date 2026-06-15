4cffc6ca81f700a0dacda0fbec1d144d1ce13e55
rishabhsinghvishen3050-ui <rishabhsinghvishen3050@gmail.com>
2026-06-15 18:47:24 +0000

    hi

--- 01_basics/05_datatypes-revsion.js
+++ 01_basics/05_datatypes-revsion.js
@@ -15,3 +15,20 @@ const myFunction=function(){
     console.log("Risabh")
 }
 myFunction()
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// STACK (PRIMITIVE)  HEAP (REFRENCE)
+
+myNAme="Risahbh"
+secondName=myNAme
+secondName="Aryan"
+console.log(myNAme==secondName)
+
+let obj1={
+    email:"rishabh@gmail.com",
+    pass:12345
+}
+obj2=obj1
+obj2.email="aryan@gmail.com"
+console.log(obj1==obj2)
